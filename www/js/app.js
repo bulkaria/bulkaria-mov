c96cfb117687c74388ad647605b28e7d26d3a3f8
fbrinkworth <federico.brinkworth@gmail.com>
2015-03-19 19:01:33 -0300

    Reset Password

--- www/js/app.js
+++ www/js/app.js
@@ -78,7 +78,7 @@ angular.module('bulkaria-mov', ['ionic', 'firebase', 'angularMoment', 'bulkaria-
   $stateProvider
 
   // State to represent Login View
-    .state('login', {
+  .state('login', {
     url: "/login",
     templateUrl: "templates/login.html",
     controller: 'LoginCtrl',
@@ -86,7 +86,7 @@ angular.module('bulkaria-mov', ['ionic', 'firebase', 'angularMoment', 'bulkaria-
       // controller will not be loaded until $waitForAuth resolves
       // Auth refers to our $firebaseAuth wrapper in the example above
       "currentAuth": ["Auth",
-                function (Auth) {
+        function (Auth) {
           // $waitForAuth returns a promise so the resolve waits for it to complete
           return Auth.$waitForAuth();
         }]
@@ -102,11 +102,11 @@ angular.module('bulkaria-mov', ['ionic', 'firebase', 'angularMoment', 'bulkaria-
       // controller will not be loaded until $requireAuth resolves
       // Auth refers to our $firebaseAuth wrapper in the example above
       "currentAuth": ["Auth",
-                function (Auth) {
+        function (Auth) {
           // $requireAuth returns a promise so the resolve waits for it to complete
           // If the promise is rejected, it will throw a $stateChangeError (see above)
           return Auth.$requireAuth();
-                }]
+        }]
     }
   })
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,47 +5,49 @@ angular.module('bulkaria-mov.controllers', [])
 
   var ref = new Firebase($scope.firebaseUrl);
   var auth = $firebaseAuth(ref);
-  $scope.user = {email: 'test1@foo.com', pwdForLogin: '1234'};
+  $scope.user = {
+    email: 'test1@foo.com',
+    pwdForLogin: '1234'
+  };
 
-  $ionicModal.fromTemplateUrl('templates/signup.html', {
-    scope: $scope
-  }).then(function (modal) {
-    $scope.modal = modal;
-  });
+  //$ionicModal.fromTemplateUrl('templates/signup.html', {
+  //  scope: $scope
+  //}).then(function (modal) {
+  //  $scope.modal = modal;
+  //});
 
-  $scope.createUser = function (user) {
-    console.log("Create User Function called");
-    if (user && user.email && user.password && user.displayname) {
-      //$ionicLoading.show({
-      //  template: 'Signing Up...'
-      //});
-      $ionicLoading.show();
+  //$ionicModal.fromTemplateUrl('templates/modal-on-login.html', {
+  //  scope: $scope,
+  //  animation: 'slide-in-up'
+  //}).then(function (modal) {
+  //  $scope.modal = modal;
+  //  $scope.showModalContent = true;
+  //});
 
-      auth.$createUser({
-        email: user.email,
-        password: user.password
-      }).then(function (userData) {
-        alert("User created successfully!");
-        ref.child("users").child(userData.uid).set({
-          email: user.email,
-          displayName: user.displayname
-        });
-        $ionicLoading.hide();
-        $scope.modal.hide();
-      }).catch(function (error) {
-        alert("Error: " + error);
-        $ionicLoading.hide();
-      });
-    } else
-      alert("Please fill all details");
-  }
+  //$scope.modalShow = function (modalForm) {
+  //  switch (modalForm) {
+  //  case "signup":
+  //    $scope.modalTemplate = 'templates/signup.html'
+  //    break;
+  //  case "resetPassword":
+  //    $scope.modalTemplate = 'templates/reset-password.html'
+  //  }
+  //  $scope.modal.show();
+  //};
 
+  // patch for modal refresh
+  //$scope.$on('modal.hidden', function() {
+  //  $scope.showModalContent = false;
+  //});
+  //
+  //$scope.$on('modal.shown', function() {
+  //  setTimeout(function () {
+  //    $scope.showModalContent = true
+  //  }, 500);
+  //});
+  
   $scope.signIn = function (user) {
-
     if (user && user.email && user.pwdForLogin) {
-      //$ionicLoading.show({
-      //  template: 'Signing In...'
-      //});
       $ionicLoading.show();
       auth.$authWithPassword({
         email: user.email,
@@ -67,7 +69,70 @@ angular.module('bulkaria-mov.controllers', [])
       });
     } else
       alert("Please enter email and password both");
+  };
+
+  $scope.signUp = function() {
+    $ionicModal.fromTemplateUrl('templates/signup.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal = modal;
+      modal.show();
+    });    
+  };
+  
+  $scope.createUser = function (user) {
+    console.log("Create User Function called");
+    if (user && user.email && user.password && user.displayname) {
+      $ionicLoading.show();
+
+      auth.$createUser({
+        email: user.email,
+        password: user.password
+      }).then(function (userData) {
+        alert("User created successfully!");
+        ref.child("users").child(userData.uid).set({
+          email: user.email,
+          displayName: user.displayname
+        });
+        $ionicLoading.hide();
+        $scope.modal.hide();
+      }).catch(function (error) {
+        alert("Error: " + error);
+        $ionicLoading.hide();
+      });
+    } else
+      alert("Please fill all details");
   }
+  
+  $scope.forgotPassword = function () {
+    $ionicModal.fromTemplateUrl('templates/reset-password.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function (modal) {
+      $scope.modal = modal;
+      modal.show();
+    });
+  };
+  
+  $scope.resetPassword = function (resetEmail) {
+    if (resetEmail) {
+      $ionicLoading.show();
+      ref.resetPassword({
+        email : resetEmail
+        }, function(error) {
+          if (error === null) {
+            console.log("Password reset email sent successfully");
+          } else {
+            console.log("Error sending password reset email:", error);
+          }
+        });
+      $ionicLoading.hide();
+      $scope.modal.hide();
+    } else
+      alert("Please fill email");
+  }
+  
 })
 
 .controller('ChatCtrl', function ($scope, Chats, $state) {
